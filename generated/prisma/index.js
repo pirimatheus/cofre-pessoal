763d64ff6d01318f437e632d34ac66defc3a988d
pirimatheus <matheuspirenopolis@gmail.com>
2026-07-18 20:37:15 -0300

    fix: regenerar prisma client com PushSubscription

--- generated/prisma/index.js
+++ generated/prisma/index.js
@@ -239,7 +239,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
+    "rootEnvPath": "../../.env",
     "schemaEnvPath": "../../.env"
   },
   "relativePath": "../../prisma",
--- generated/prisma/wasm.js
+++ generated/prisma/wasm.js
@@ -238,7 +238,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
+    "rootEnvPath": "../../.env",
     "schemaEnvPath": "../../.env"
   },
   "relativePath": "../../prisma",
